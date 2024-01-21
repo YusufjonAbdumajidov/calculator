ef7d85ff0e51aaba39ab25b14f502378c412d70e
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2024-01-21 19:56:43 +0100

    bugs fixed and new changes applied

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,4 @@
-import { useContext } from "react";
 import { TouchableOpacity, Text } from "react-native";
-import { ThemeContext } from "../context/ThemeContext";
 import { Styles } from "../styles/GlobalStyles";
 
 interface ButtonProps {
@@ -11,26 +9,21 @@ interface ButtonProps {
 }
 
 export default function Button({ title, onPress, isBlue, isGray }: ButtonProps) {
-    const theme = useContext(ThemeContext);
     return (
         <TouchableOpacity 
             style={
                 isBlue 
                 ? Styles.btnBlue 
                 : isGray 
-                ? Styles.btnGray 
-                : theme === "light" 
-                ? Styles.btnLight 
-                : Styles.btnDark
+                ? Styles.btnGray : Styles.btnLight
             } 
             onPress={onPress}>
             <Text 
                style={
                    isBlue || isGray 
                    ? Styles.smallTextLight
-                   : theme === "dark" 
                    ? Styles.smallTextLight 
-                   : Styles.smallTextDark 
+                   : Styles.smallTextDark : Styles.smallTextDark
                 }
             >
                 {title}
